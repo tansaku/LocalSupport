d95ee9ee70eb1deff55d05493dd1fad7fe767c51
Pavel Osadchyi <posadchiy@gmail.com>
2013-05-09 20:54:23 +0300

    Organization with failed address is saved now. Steps and scenario was created by Michael (mtc2013)

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,6 +1,23 @@
 class Organization < ActiveRecord::Base
   acts_as_gmappable :check_process => false
 
+  def remove_errors_with_address
+    errors_hash = errors.to_hash
+    errors.clear
+    errors_hash.each do |key, value|
+      if key.to_s != 'gmaps4rails_address'
+        errors.add(key, value)
+      end
+    end
+  end
+
+  #This method is overridden to save organization if address was failed to geocode
+  def run_validations!
+    run_callbacks :validate
+    remove_errors_with_address
+    errors.empty?
+  end
+
   def self.search_by_keyword(keyword)
     self.where("description LIKE ?","%#{keyword}%")
   end
@@ -13,60 +30,58 @@ class Organization < ActiveRecord::Base
     "#{self.name}"
   end
 
+  # def self.create_from_text_file(filename, limit)
+    #csv_text = File.open(filename, 'r:ISO-8859-1')
+    #count = 0
+    #CSV.parse(csv_text, :headers => true).each do |row|
+		  #if count > limit
+			  #break
+		  #end
+		  #count += 1
+		  #self.create_from_text(row)
+		#end
+
+#.take
+  # end
+
   def self.humanize_description(unfriendly_description) 
     unfriendly_description && unfriendly_description.humanize
   end
-
-  def self.parse_address(address_with_trailing_postcode)  
-    address = address_with_trailing_postcode.to_s
-    postcode = ''
-    match = address_with_trailing_postcode.to_s.match(/(.*)(,\s*(\w\w\d\s* \d\w\w))/)
-    if match
-      if match.length == 4 
-        address = match[1]
-        postcode = match[3].to_s
-      end
-    end
-    {:address => address, :postcode => postcode}
+  def self.extract_postcode(address_with_trailing_postcode)  
+    match = address_with_trailing_postcode && address_with_trailing_postcode.match(/\s*(\w\w\d\s* \d\w\w)/)
+    match && match[1]
   end
 
-  #Title,Charity Number,Activities,Contact Name,Contact Address,website,Contact Telephone,date registered,date removed,accounts date,spending,income,company number,OpenlyLocalURL,twitter account name,facebook account name,youtube account name,feed url,Charity Classification,signed up for 1010,last checked,created at,updated at,Removed?
+  def self.create_from_text(csv_string)
+    parsed = CSV.parse(csv_string)
 
-  @@NAME_COLUMN = 0
-  @@DESCRIPTION_COLUMN = 2
-  @@ADDRESS_COLUMN = 4
-  @@WEBSITE_COLUMN = 5
-  @@TELEPHONE_COLUMN = 6
-  @@DATE_REMOVED_COLUMN = 8
+    description = parsed[0][1]
+    description = self.humanize_description(description)
 
-  def self.create_from_array(array)
-    return nil if array[@@DATE_REMOVED_COLUMN]
+    name = parsed[0][0] 
+    name = name.humanized_all_first_capitals if name
 
-    address = self.parse_address(array[@@ADDRESS_COLUMN])
+    address = parsed[0][2] && parsed[0][2].sub(/,\s*\w\w\d\s* \d\w\w/,"")
+    address = address.humanized_all_first_capitals if address
 
-    self.create :name => array[@@NAME_COLUMN].to_s.humanized_all_first_capitals,
-		:description => self.humanize_description(array[@@DESCRIPTION_COLUMN]),
-		:address => address[:address].humanized_all_first_capitals,
-                :postcode => address[:postcode],
-		:website => array[@@WEBSITE_COLUMN],
-		:telephone => array[@@TELEPHONE_COLUMN]
+    postcode = self.extract_postcode(parsed[0][2])
+    #tokens = string.split(',')
+    self.create :name => name,
+		:description => description,
+		:address => address,
+                :postcode => postcode,
+		:website => parsed[0][3],
+		:telephone => parsed[0][4]
+
   end
 
-  def self.import_addresses(filename,limit)
-    csv_text = File.open(filename, 'r:ISO-8859-1')
-    count = 0
-    CSV.parse(csv_text, :headers => true).each do |row|
-		  if count >= limit
-	            break
-		  end
-		  count += 1
-		  self.create_from_array(row)
-    end
-  end
+  #def self.import_build_addresses(filename)
+
+  #end
 end
 
 class String
   def humanized_all_first_capitals
-    self.to_s.humanize.split(' ').map{|w| w.capitalize}.join(' ')
+    self.humanize.split(' ').map{|w| w.capitalize}.join(' ')
   end
 end
