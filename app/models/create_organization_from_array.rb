3c2478dacdf4e3b4b07349eda1892f91c5c08c49
Rafael George <george.rafael@gmail.com>
2013-09-03 09:27:48 -0400

    Fix failing specs due to some merge symbols inside CreateOrganizationFromArray

--- app/models/create_organization_from_array.rb
+++ app/models/create_organization_from_array.rb
@@ -2,7 +2,6 @@ class CreateOrganizationFromArray
 
   def initialize(row, mappings)
     @row = row
-<<<<<<< HEAD
     @mappings = normalize(mappings)
   end
 
@@ -11,42 +10,23 @@ class CreateOrganizationFromArray
     return nil if Organization.find_by_name(organization_name)
 
     org = build_organization(organization_name)
-=======
-    @mappings = mappings
-  end
-
-  def call(validate)
-    check_columns_in
-    organization_name = FirstCapitalsHumanizer.call(@row[@mappings[:name]])
-    return nil if @row[@mappings[:date_removed]]
-    return nil if Organization.find_by_name(organization_name)
-
-    org = build_organization_from_array(organization_name)
->>>>>>> 46f38c9b51bd82ca19bff1a3fc59bab11e1c5ebb
-
     org.save! validate: validate
     org
   end
 
   private
-<<<<<<< HEAD
   def organization_name
     @organization_name ||= FirstCapitalsHumanizer.call(@row[@mappings[:name]])
   end
 
   def normalize(mappings)
     mappings.each_value do |column_name|
-=======
-  def check_columns_in
-    @mappings.each_value do |column_name|
->>>>>>> 46f38c9b51bd82ca19bff1a3fc59bab11e1c5ebb
       unless @row.header?(column_name)
         raise CSV::MalformedCSVError, "No expected column with name #{column_name} in CSV file"
       end
     end
   end
 
-<<<<<<< HEAD
   def build_organization(organization_name)
     address = Address.new(@row[@mappings[:address]]).parse
     org = Organization.new({
@@ -57,18 +37,6 @@ class CreateOrganizationFromArray
       website: @row[@mappings[:website]],
       telephone: @row[@mappings[:telephone]]
     })
-=======
-  def build_organization_from_array(organization_name)
-    org = Organization.new
-    address = Address.new(@row[@mappings[:address]]).parse
-    org.name = organization_name
-    org.description = DescriptionHumanizer.call((@row[@mappings[:description]]))
-    org.address = FirstCapitalsHumanizer.call(address[:address])
-    org.postcode = address[:postcode]
-    org.website = @row[@mappings[:website]]
-    org.telephone = @row[@mappings[:telephone]]
-    return org
->>>>>>> 46f38c9b51bd82ca19bff1a3fc59bab11e1c5ebb
   end
 
 end
