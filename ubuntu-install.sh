99600b3113481411142cc2cfd811db06c9e2583f
Michael Smith <mikesmithnova@gmail.com>
2013-12-03 18:35:18 -0400

    Updated ubuntu install script

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -4,53 +4,85 @@
 # cd LocalSupport and
 # run with . <filename>.sh
 
-# Get password to be used with sudo commands
-echo -n "Enter password to be used for sudo commands:"
-read -s password
+# OPTIONS: Useful if you have to rerun the script after an error
+# NOTE: Most things are ok to re-run except possibly DB create
+#       Db seed tasks and DB peer authentication fix
+#   no_rvm_ruby - Do not try to update RVM and Ruby
+#   no_package - Do not install aditional packages
+#   no_bundle - Do not run bundle and selenium installs
+#   no_peer_fix - Do not apply peer fix
+#   no_db_create - Do not create database, useful if DB exists
+#   no_db_seed - Do not run seeding command, useful if already seeded
 
-# Function to issue sudo command with password
-function sudo-pw {
-    echo $password | sudo -S $@
+# Function for standard error message
+function error {
+  echo "ERROR: Failed to $1, please fix the issue"
+  echo "       and run the script again"
+  echo "NOTE: You can optionally skip completed sections with"
+  echo "      arguments listed at the top of this file."
 }
 
 # Update RVM and Ruby
-echo Y | rvm get stable
-rvm reload
-echo Y | rvm upgrade 1.9.3
+if [[ $@ != *no_rvm_ruby* ]]; then
+  echo Y | rvm get stable || { error "update RVM"; return 1; }
+  rvm reload || error "reload RVM"
+  echo Y | rvm upgrade 1.9.3 || { error "upgrade Ruby"; return 1; }
+fi
 
-# Install Qt webkit headers
-sudo-pw apt-get install -y libqtwebkit-dev
+if [[ $@ != *no_package* ]]; then
+  # Install Qt webkit headers
+  sudo apt-get install -y libqtwebkit-dev || { error "install webkit dev"; return 1; }
 
-# Install postgreSQL
-sudo-pw apt-get install -y libpq-dev
-sudo-pw apt-get install -y postgresql
+  # Install postgreSQL
+  sudo apt-get install -y libpq-dev || { error "install pg dev"; return 1; }
+  sudo apt-get install -y postgresql || { error "install pg"; return 1; }
 
-# Install X virtual frame buffer
-sudo-pw apt-get install -y xvfb
+  # Install X virtual frame buffer
+  sudo apt-get install -y xvfb || { error "install xvfb"; return 1; }
 
-# Remove un-needed packages
-sudo-pw apt-get -y autoremove
+  # Remove un-needed packages
+  sudo apt-get -y autoremove
+fi
 
 # git checkout develop
-git checkout develop
+git checkout develop || { error "checkout develop branch"; return 1; }
 
 # Run bundle install to get the gems
-bundle install
-selenium install
+if [[ $@ != *no_bundle* ]]; then
+  bundle install || { error "bundle install"; return 1; }
+  selenium install || { error "install selenium"; return 1; }
+fi
+
+# Apply peer authentication fix, assumes saasbook username
+# Change below if necessary and commit change before running script
+if [[ $@ != no_peer_fix ]]; then
+  sudo sed -i 's/local[ ]*all[ ]*postgres[ ]*peer/local all postgres peer map=basic/' /etc/postgresql/9.1/main/pg_hba.conf || { error "apply peer fix part 1"; return 1; }
+  sudo sed -i '$ a\basic saasbook postgres' /etc/postgresql/9.1/main/pg_ident.conf || { error "apply peer fix part 2"; return 1; }
+fi
+
+sudo /etc/init.d/postgresql restart
 
 # Run the following to get the database set up and import seed data
-bundle exec rake db:create
-bundle exec rake db:migrate
-echo "Please wait, seeding database, this could take a while ..."
-bundle exec rake db:categories
-bundle exec rake db:seed
-bundle exec rake db:cat_org_import
-bundle exec rake db:pages
+if [[ $@ != *no_db_create* ]]; then
+  bundle exec rake db:create || { error "create DB"; return 1; }
+fi
+bundle exec rake db:migrate || { error "migrate DB"; return 1; }
+if [[ $@ != *no_db_seed* ]]; then
+  echo "Please wait, seeding database, this could take a while ..."
+  bundle exec rake db:categories || { error "add categories to DB"; return 1; }
+  bundle exec rake db:seed || { error "seed DB"; return 1; }
+  bundle exec rake db:cat_org_import || { error "add org to DB"; return 1; }
+  bundle exec rake db:pages || { error "add pages to DB"; return 1; }
+fi
+
 bundle exec rake db:test:prepare
 
 echo "**** NOTICE ****"
 echo "SETUP COMPLETE"
-echo "You can now run rails s to start a local server. See db/seeds.rb for user info"
-echo "You can run the following commands to test your setup. All tests should pass"
+echo "You can now run rails s to start a local server."
+echo "See db/seeds.rb for user info"
+echo "You can run the following commands to test your setup."
+echo "All tests should pass"
 echo "bundle exec rake spec"
 echo "bundle exec rake cucumber"
+
