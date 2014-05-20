09a63994db0206cb484d396dd50c83d389bfa857
Sam Joseph <tansaku@gmail.com>
2014-05-20 16:49:13 +0100

    this is broken, but is an example refactoring

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -3,19 +3,35 @@ class InvitationsController < ApplicationController
 
   # xhr only, tested in a request spec
   def create
-    flag = params.fetch(:resend_invitation).to_s == 'true' ? true : false
-    Devise.resend_invitation = flag
-    answers = params.fetch(:values).map do |invite|
-      user = User.invite!({email: invite.fetch(:email)}, current_user) do |user|
-        user.organization_id = invite.fetch(:id)
-      end
-      answer = if user.errors.any?
-                   user.errors.full_messages.map{|msg| "Error: #{msg}"}.join(' ')
-                 else
-                   'Invited!'
-                 end
-      {invite.fetch(:id) => answer}
+    tell_devise_if_okay_to_resend_invitations
+    invites = get_invitations
+    invites.each do |organization_id, email|
+       user = invite_user(email, organization_id)
+       invites[organization_id] = check_result_of_inviting user
     end
-    render json: answers.reduce({}, :update).to_json
+    render json: invites.to_json
+  end
+
+  def tell_devise_if_okay_to_resend_invitations
+    flag = params.fetch(:resend_invitation).to_s == 'true'
+    Devise.resend_invitation = flag
+  end
+
+  def invite_user email, organization_id
+    User.invite!({email: email}) do |user|
+      user.organization_id = organization_id
+    end
+  end
+
+  def check_result_of_inviting user
+    if user.errors.any?
+      user.errors.full_messages.map{|msg| "Error: #{msg}"}.join(' ')
+    else
+      'Invited!'
+    end
+  end
+
+  def get_invitations
+    params.fetch(:values).reduce({}, :update)
   end
 end
